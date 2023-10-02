f6e60a1c3872720f945a93656455761fb08c3c22
Miguel Lisboa <miguelflisboa@outlook.com>
2023-10-02 20:34:48 -0300

    Position class created.

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Position P;
+
+			P = new Position(3, 2);
+
+			Console.WriteLine("Position: " +  P);
+
+			Console.ReadLine();
+
+		}
+	}
+}

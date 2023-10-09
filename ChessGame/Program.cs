2b4e4b3afed0e533a2c201d237591778b9b04a72
Miguel Lisboa <miguelflisboa@outlook.com>
2023-10-08 23:05:39 -0300

    Creating the ChessPosition class

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -15,20 +15,24 @@ namespace ChessGame
             try
             {
                 Board board = new Board(8, 8);
-                board.alocatePiece(new Tower(board, Color.Black), new Position(0, 0));
+
+                ChessPosition cp1 = new ChessPosition('a', 8);
+                ChessPosition cp2 = new ChessPosition('b', 6);
+
+                board.alocatePiece(new Tower(board, Color.Black), cp1.toPosition());
                 //board.alocatePiece(new Tower(board, Color.White), new Position(0, 0));
-                board.alocatePiece(new King(board, Color.White), new Position(1, 3));
+                board.alocatePiece(new King(board, Color.White), cp2.toPosition());
 
 
                 Screen.printBoard(board);
 
             }
-            catch (BoardException e) 
+            catch (BoardException e)
             {
                 Console.WriteLine(e.Message);
             }
-            
-            
+
+
             Console.ReadLine();
 
         }
---
+++ ChessGame/chess/ChessPosition.cs
@@ -0,0 +1,32 @@
+﻿using board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess
+{
+    internal class ChessPosition
+    {
+        public char column { get; set; }
+        public int row { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            this.column = column;
+            this.row = row;
+        }
+
+        public Position toPosition()
+        {
+            return new Position( 8 - this.row, this.column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + column + row;
+        }
+
+    }
+}
